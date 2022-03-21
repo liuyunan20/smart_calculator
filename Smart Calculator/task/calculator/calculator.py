fff497b799fecc28e35b0de91e5e7e1111884699
liuyunan <liuyunan.20@gmail.com>
2022-03-21 00:11:16 -0700

    Add to the calculator the ability to read an unlimited sequence of numbers. Add a /help command to print some information about the program. If you encounter an empty line, do not output anything.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -3,6 +3,9 @@ while True:
     if user_in == "/exit":
         print("Bye!")
         break
+    if user_in == "/help":
+        print("The program calculates the sum of numbers")
+        continue
     if not user_in:
         continue
     nums = [int(x) for x in user_in.split(" ")]
