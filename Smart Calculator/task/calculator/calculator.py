96a83b9f9138d8908827c00578d3d2a3505cb41e
liuyunan <liuyunan.20@gmail.com>
2022-03-21 00:07:10 -0700

    Write a program that reads two numbers in a loop and prints the sum in the standard output. If a user enters only a single number, the program should print the same number. If a user enters an empty line, the program should ignore it. When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,2 +1,9 @@
-a, b = input().split()
-print(int(a) + int(b))
+while True:
+    user_in = input()
+    if user_in == "/exit":
+        print("Bye!")
+        break
+    if not user_in:
+        continue
+    nums = [int(x) for x in user_in.split(" ")]
+    print(sum(nums))
