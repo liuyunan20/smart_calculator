4453d50da16bff8fdec2ba11e6db5748d9e41a85
liuyunan <liuyunan.20@gmail.com>
2022-03-21 13:14:45 -0700

    The program must calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on. Modify the result of the /help command to explain these operations. Decompose your program using functions to make it easy to understand and edit later. The program should not stop until the user enters the /exit command. If you encounter an empty line, do not output anything.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,12 +1,34 @@
+# process input to available parameters
+def process_input(ipt):
+    ipt = ipt.split()
+    nums = []
+    for x in ipt:
+        try:
+            x = int(x)
+            nums.append(x)
+        except ValueError:
+            x = list(x)
+            if x[0] == "-":
+                if len(x) % 2 == 1:
+                    nums.append("-")
+    for i, num in enumerate(nums):
+        if num == "-":
+            nums[i] = 0
+            nums[i + 1] = -nums[i + 1]
+    return nums
+
+
+help_information = '''The program operate addition and subtraction of numbers.
+If the user has entered several same operators following each other, the program still work.
+It support both unary and binary minus operators.'''
 while True:
     user_in = input()
     if user_in == "/exit":
         print("Bye!")
         break
     if user_in == "/help":
-        print("The program calculates the sum of numbers")
+        print(help_information)
         continue
     if not user_in:
         continue
-    nums = [int(x) for x in user_in.split(" ")]
-    print(sum(nums))
+    print(sum(process_input(user_in)))
